4d00d8ab718aeeefbfe329d8fb56ac476bae5d32
Valentine Omonya <valomosh254@gmail.com>
2026-06-28 03:59:40 +0300

    style: replace logo with lazy downloader pixel art character

--- packages/lazydlp/source/components/logo.tsx
+++ packages/lazydlp/source/components/logo.tsx
@@ -4,12 +4,15 @@ import { theme } from '../theme.js';
 
 export default function Logo() {
 	return (
-		<Box flexDirection="column" alignItems="center">
-			<Box>
-				<Text color={theme.highlight}>▶ </Text>
-				<Text color={theme.link}>▼ </Text>
-			</Box>
-			<Text color={theme.success} bold>yt-dlp</Text>
+		<Box flexDirection="column" alignItems="center" marginY={1}>
+			<Text color={theme.link}>   ████████   </Text>
+			<Text color={theme.link}> ██        ██ </Text>
+			<Text color={theme.link}> ██ ▀▀  ▀▀ ██ </Text>
+			<Text color={theme.link}>██████████████</Text>
+			<Text color={theme.link}>████  ██  ████</Text>
+			<Text color={theme.link}>  ██████████  </Text>
+			<Text color={theme.link}>    ██████    </Text>
+			<Text color={theme.link}>      ██      </Text>
 		</Box>
 	);
 }
